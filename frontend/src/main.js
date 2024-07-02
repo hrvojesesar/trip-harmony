f7f5f905d9777c61bd11da3408dd9b5712954954
Franjo Lovrić <franjolovric23@gmail.com>
2024-07-02 12:40:11 +0200

    Integrate Google Maps with autocomplete for location input

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(VueGoogleMaps, {
+    load: {
+        key: import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY,
+        libraries: 'places'
+    }
+})
 
 app.mount('#app')
--- frontend/src/views/LocationView.vue
+++ frontend/src/views/LocationView.vue
@@ -5,8 +5,11 @@
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
-                        <input type="text" placeholder="My destination"
-                        class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+                        <GMapAutocomplete 
+                            placeholder="My destination"
+                            @place_changed="handleLocationChanged"
+                            class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+                        </GMapAutocomplete>
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
@@ -20,3 +23,11 @@
         </form>
     </div>
 </template>
+
+<script setup>
+
+const handleLocationChanged = (e) => {
+    console.log('handleLocationChanged', e)
+} 
+
+</script>
