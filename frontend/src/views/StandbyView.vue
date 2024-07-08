2e11066f4f38df5bb5825e48ac121cc4cf8601ae
Franjo Lovrić <franjolovric23@gmail.com>
2024-07-08 10:07:22 +0200

    Design a waiting area for drivers to receive ride requests

--- frontend/src/views/StandbyView.vue
+++ frontend/src/views/StandbyView.vue
@@ -1,7 +1,12 @@
 <template>
-
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Waiting for a ride request...</h1>
+        <div class="mt-8 flex justify-center">
+            <Loader />
+        </div>
+    </div>
 </template>
 
 <script setup>
-
+import Loader from '@/components/Loader.vue';
 </script>
