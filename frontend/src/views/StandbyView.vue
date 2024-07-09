b93379cd5680522dcbde1cf844b9a19b97b3b557
Franjo Lovrić <franjolovric23@gmail.com>
2024-07-09 09:24:01 +0200

    Implement driver interface with map showing passenger's destination and add accept/reject ride buttons

--- frontend/src/views/StandbyView.vue
+++ frontend/src/views/StandbyView.vue
@@ -1,8 +1,29 @@
 <template>
     <div class="pt-16">
-        <h1 class="text-3xl font-semibold mb-4">Waiting for a ride request...</h1>
-        <div class="mt-8 flex justify-center">
+        <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
+        <div v-if="!trip.id" class="mt-8 flex justify-center">
             <Loader />
+        </div>
+        <div v-else>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <GMapMap :zoom="14" :center="trip.destination" ref="gMap"
+                            style="width:100%; height: 256px;"></GMapMap>
+                    </div>
+                    <div class="mt-2">
+                        <p class="text-xl">Going to <strong>{{ trip.destination_name }}</strong></p>
+                    </div>
+                </div>
+                <div class="flex justify-between bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                        @click="handleDeclineTrip"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Decline</button>
+                    <button
+                        @click="handleAcceptTrip"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Accept</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -12,6 +33,11 @@ import Loader from '@/components/Loader.vue';
 import Echo from 'laravel-echo';
 import { onMounted } from 'vue';
 import Pusher from 'pusher-js';
+import { useTripStore } from '@/stores/trip';
+import { ref } from 'vue';
+
+const title = ref('Waiting for a ride request...');
+const trip = useTripStore();
 
 onMounted(() => {
    let echo = new Echo({
@@ -27,6 +53,8 @@ onMounted(() => {
 
     echo.channel('drivers')
          .listen('TripCreated', (e) => {
+              title.value = 'Ride request received!';  
+              trip.$patch(e.trip)
               console.log('TripCreated', e);
          });
 });
