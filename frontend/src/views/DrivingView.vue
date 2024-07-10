1fdbd942d82328201c0be079ea91b5d7cd251923
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-07-10 14:54:13 +0200

    Added complete trip and picked up passenger logic

--- frontend/src/views/DrivingView.vue
+++ frontend/src/views/DrivingView.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="pt-16">
-        <h1 class="text-3xl font-semibold mb-4">Driving to your passenger...</h1>
+        <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
      <div>
-        <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+        <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left" v-if="!trip.is_complete">
             <div class="bg-white px-4 py-5 sm:p-6">
                 <div>
                     <GMapMap :zoom="14" :center="location.current.geometry" ref="gMap"
@@ -17,16 +17,23 @@
             </div>
             <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
                 <button v-if="trip.is_started"
+                        @click="handleCompleteTrip"
                         class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
                         Complete Trip
                     </button>
                     <button v-else
+                        @click="handlePassengerPickedUp"
                         class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
                         Start Trip
                     </button>
+                </div>
+            </div>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left" v-else>
+                <div class="bg-white px-4 py-5 sm:p-6">
+                  <Tada />
+                </div>
             </div>
         </div>
-      </div>
     </div>
 </template>
 
@@ -38,12 +45,18 @@ import { http } from '@/helpers/http';
 import axios from 'axios';
 import Pusher from 'pusher-js';
 import Echo from 'laravel-echo';
+import Tada from '@/components/Tada.vue'
+import { useRouter } from 'vue-router'
 
 const location = useLocationStore()
 const trip = useTripStore()
+const router = useRouter()
 
 const gMap = ref(null);
 const intervalRef = ref(null);
+
+
+const title = ref("Driving to passenger....")
 
 const currentIcon = {
     url: 'https://openmoji.org/data/color/svg/1F698.svg',
@@ -80,6 +93,44 @@ const broadcastDriverLocation = () => {
     .catch((error) => {
         console.error(error)
     })
+}
+
+const handlePassengerPickedUp = () => {
+    http().post(`/api/trip/${trip.id}/start`)
+        .then((response) => {
+            title.value = 'Travelling to destination...'
+            location.$patch({
+                destination: {
+                    name: response.data.destination_name,
+                    geometry: response.data.destination
+                }
+            })
+            trip.$patch(response.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
+
+const handleCompleteTrip = () => {
+    http().post(`/api/trip/${trip.id}/end`)
+        .then((response) => {
+            title.value = 'Trip completed!'
+
+            trip.$patch(response.data)
+
+            setTimeout(() => {
+                trip.reset()
+                location.reset()
+
+                router.push({
+                    name: 'standby'
+                })
+            }, 3000)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 }
 
 onMounted(() => {
