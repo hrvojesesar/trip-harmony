f799bda6928b2767c675f665b825eabfd1015ff2
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-07-24 16:40:05 +0200

    Added new attributes

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->string('surname')->nullable();
             $table->string('phone')->unique();
             $table->string('login_code')->nullable();
             $table->rememberToken();
--- backend/database/migrations/2024_06_26_111227_create_drivers_table.php
+++ backend/database/migrations/2024_06_26_111227_create_drivers_table.php
@@ -15,11 +15,24 @@ return new class extends Migration
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class);
-            $table->integer('year');
-            $table->string('make');
-            $table->string('model');
-            $table->string('color');
-            $table->string('license_plate');
+            $table->string('name');
+            $table->string('surname');
+            $table->date('birth_date');
+            $table->string('OIB');
+            $table->string('serial_number_identity_card');
+            $table->string('URL_identity_card_front')->nullable();
+            $table->string('URL_identity_card_back')->nullable();
+            $table->string('serial_number_driver_license');
+            $table->string('URL_driver_license_front')->nullable();
+            $table->string('URL_driver_license_back')->nullable();
+            $table->string('serial_number_health_card');
+            $table->string('URL_health_card_front')->nullable();
+            $table->string('URL_health_card_back')->nullable();
+            $table->string('Car_name');
+            $table->string('Car_model');
+            $table->string('Car_color');
+            $table->string('registration_mark');
+            $table->string('URL_registration_certificate')->nullable();
             $table->timestamps();
         });
     }
