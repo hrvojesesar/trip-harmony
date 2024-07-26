561e74461f17b26c6da8f96a63272a489663f9f0
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-07-26 13:30:18 +0200

    Upgraded view

--- frontend/src/views/DriverView.vue
+++ frontend/src/views/DriverView.vue
@@ -5,46 +5,57 @@
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
+                        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
                         <input type="text" name="name" id="name" v-model="driverDetails.name" placeholder="Name"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="surname" class="block text-sm font-medium text-gray-700">Surname</label>
                         <input type="text" name="surname" id="surname" v-model="driverDetails.surname" placeholder="Surname"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
-                        <input type="date" name="birth_date" id="birth_date" v-model="driverDetails.birth_date" placeholder="Birth Date"
+                        <label for="birth_date" class="block text-sm font-medium text-gray-700">Date of Birth</label>
+                        <input type="date" name="birth_date" id="birth_date" v-model="driverDetails.birth_date" placeholder="dd. mm. gggg."
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="OIB" class="block text-sm font-medium text-gray-700">OIB</label>
                         <input type="text" name="OIB" id="OIB" v-model="driverDetails.OIB" placeholder="OIB"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="serial_number_identity_card" class="block text-sm font-medium text-gray-700">Identity Card Serial Number</label>
                         <input type="text" name="serial_number_identity_card" id="serial_number_identity_card" v-model="driverDetails.serial_number_identity_card" placeholder="Identity Card Serial Number"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="serial_number_driver_license" class="block text-sm font-medium text-gray-700">Driver License Serial Number</label>
                         <input type="text" name="serial_number_driver_license" id="serial_number_driver_license" v-model="driverDetails.serial_number_driver_license" placeholder="Driver License Serial Number"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="serial_number_health_card" class="block text-sm font-medium text-gray-700">Health Card Serial Number</label>
                         <input type="text" name="serial_number_health_card" id="serial_number_health_card" v-model="driverDetails.serial_number_health_card" placeholder="Health Card Serial Number"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="Car_name" class="block text-sm font-medium text-gray-700">Car Name (Big Letters)</label>
                         <input type="text" name="Car_name" id="Car_name" v-model="driverDetails.Car_name" placeholder="Car Name (Big Letters)"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="Car_model" class="block text-sm font-medium text-gray-700">Car Model (Big Letters)</label>
                         <input type="text" name="Car_model" id="Car_model" v-model="driverDetails.Car_model" placeholder="Car Model (Big Letters)"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="Car_color" class="block text-sm font-medium text-gray-700">Car Color (Big Letters)</label>
                         <input type="text" name="Car_color" id="Car_color" v-model="driverDetails.Car_color" placeholder="Car Color (Big Letters)"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                     <div class="mt-2">
+                        <label for="registration_mark" class="block text-sm font-medium text-gray-700">Registration Mark</label>
                         <input type="text" name="registration_mark" id="registration_mark" v-model="driverDetails.registration_mark" placeholder="Registration Mark"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
@@ -65,7 +76,6 @@ import { reactive } from 'vue'
 import { http } from '@/helpers/http'
 import router from '../router';
 
-
 const driverDetails = reactive({
     name: '',
     surname: '',
@@ -84,7 +94,7 @@ const handleSaveDriver = () => {
     http().post('/api/driver', driverDetails)
         .then((response) => {
             router.push({
-                name: 'identityCard'
+                name: 'standby'
             })
         })
         .catch((error) => {
